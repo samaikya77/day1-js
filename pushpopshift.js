95699be2fede09bd978c061a7897384398ce9fc1
Samaikya Venna <sam@Samaikyas-MacBook-Air.local>
2025-02-22 00:43:52 +0100

    day1

---
+++ pushpopshift.js
@@ -0,0 +1,15 @@
+var array=['apple','banana','mango']
+var newarray=array.push('grapes'); // length=4
+console.log(newarray); //it gives the added array
+var animals=['dog','cat','monkey']
+console.log(animals.push('rat')); 
+console.log(animals);
+var cars=['maruthi','benz','audi','bmw']
+console.log(cars.pop());
+console.log(cars);
+var nums=[1,2,3,4,5]
+console.log(nums.shift())
+console.log(nums);
+var number=[1,2,3,4,5]
+console.log(number.unshift(10))
+console.log(number);
